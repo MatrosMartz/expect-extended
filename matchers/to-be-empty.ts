41b1b76ea6fe46abf57c3378a9834b06d95c4162
tsonko <MatrozMartz>
2024-11-22 17:29:16 -0600

    fix: change types import in matchers/to-be-empty.ts

--- matchers/to-be-empty.ts
+++ matchers/to-be-empty.ts
@@ -1,14 +1,14 @@
 import { format } from '@std/internal/format';
 
-import type { ExtendMatchResult, MatcherContext } from '../_types.ts';
+import type { ExtendMatchResult, MatcherContext } from '~/types';
+import { printNot } from '~/utils';
 
 function isIterable(value: object): value is Iterable<unknown> {
 	return Symbol.iterator in value && typeof value[Symbol.iterator] === 'function';
 }
 
 function isEmptyIterable(value: unknown): boolean {
-	if (typeof value !== 'object' || value == null) return false;
-	if (!isIterable(value)) return false;
+	if (value == null || !isIterable(value)) return false;
 	const firstIteration = value[Symbol.iterator]().next();
 	return firstIteration.done ?? false;
 }
@@ -18,6 +18,6 @@ export function toBeEmpty({ equal, value }: MatcherContext): ExtendMatchResult {
 
 	return {
 		pass,
-		message: () => `Expected value to ${pass ? 'not ' : ''}be empty received:\n${format(value)}`,
+		message: () => `Expected value to ${printNot(pass)}be empty received:\n${format(value)}`,
 	};
 }
