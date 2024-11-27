49632a2f12e757f2740353ec20f3dea7d54a407e
tsonko <MatrozMartz>
2024-11-26 19:25:28 -0600

    fix: correctly naming to-be-nil

--- matchers/to-be-nil.test.ts
+++ matchers/to-be-nil.test.ts
@@ -2,7 +2,7 @@ import { describe, test } from '@std/testing/bdd';
 import { expect as e } from '@std/expect';
 
 import type { ExpectedExtended } from '../expected-extended.ts';
-import * as Matchers from './to-be-nill.ts';
+import * as Matchers from './to-be-nil.ts';
 
 e.extend(Matchers);
 const expect = e<ExpectedExtended>;
@@ -15,18 +15,17 @@ describe('.toBeNil', () => {
 	test('passes when undefined is given', () => {
 		expect(undefined).toBeNil();
 	});
+
 	test('fails when the value is not null or undefined', () => {
 		expect(() => expect('value').toBeNil()).toThrow(/^Expected value to be null or undefined, received:/);
 	});
 });
 
 describe('.not.toBeNil', () => {
-	test('passes when value is not null or undefined : "true"', () => {
-		expect('true').not.toBeNil();
-	});
-
-	test('passes when value is not null or undefined : true', () => {
-		expect(true).not.toBeNil();
+	['true', {}, true].forEach((given) => {
+		test(`passes when value is not null or undefined : ${given}`, () => {
+			expect(given).not.toBeNil();
+		});
 	});
 
 	test('fails when null is given', () => {
